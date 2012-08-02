1dd85a8242f1df8bd434bafceab2c35897328b80
Swami Atma <swami@ohlalaweb.com>
2012-08-02 11:33:18 +0200

    Fixed up migration file.

--- lib/generators/tkh_content/create_migration/templates/create_pages.rb
+++ lib/generators/tkh_content/create_migration/templates/create_pages.rb
@@ -1,19 +1,21 @@
 class CreatePages < ActiveRecord::Migration
   
   def self.up
-    create_table :users do |t|
+    create_table :pages do |t|
       t.string :title
       t.text :description
       t.text :body
       t.boolean :for_blog, default: false
       t.datetime  :published_at
-
       t.timestamps
     end
+    add_index :pages, :published_at 
     Page.create_translation_table! :title => :string, :description => :text, :body => :text
   end
   
   def self.down
-    drop_table :users
+    drop_table :pages
+    Page.drop_translation_table!
+    remove_index :pages, :published_at
   end
 end
