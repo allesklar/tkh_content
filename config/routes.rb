82e9061793f51722bffc6b52c531a11512148446
Swami Atma <swami@ohlalaweb.com>
2012-08-04 20:30:29 +0200

    Added a comment to the routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     resources :pages, except: :show
+    # this is a bit funky
+    # in order to show pages without the controller name
+    # this line should be at the end of the routes file
+    # nothing to do when in context of tkh_cms
+    # working on this.
     resources :pages, only: :show, path: ''
   end
 end
