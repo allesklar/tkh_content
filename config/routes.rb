0b167445216bb579f4db83424f64a754a03997f5
Swami Atma <swami@ohlalaweb.com>
2012-08-13 16:26:31 +0200

    Undid the special route for pages show.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-    resources :pages, except: :show
+    resources :pages
   end
 end
-
-
-# this is a bit funky
-# These 3 lines must be copied by hand to the bottom of the routes file and un-commented
-
-# scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-#   resources :pages, only: :show, path: ''
-# end
