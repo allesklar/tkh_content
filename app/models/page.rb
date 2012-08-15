f01ee33a1d1d61c33f0b319f9f39daae0cdbb042
Swami Atma <swami@ohlalaweb.com>
2012-08-15 22:38:07 +0200

    Added the parent_id att_accessible attribute.

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,14 +7,14 @@ class Page < ActiveRecord::Base
   
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   
-  attr_accessible :title, :short_title, :description, :body, :blog_post, :for_blog
+  attr_accessible :title, :short_title, :description, :body, :for_blog, :parent_id
   
   validates_presence_of :title
   validates_presence_of :description
   validates_presence_of :body
   validates_presence_of :author_id
   
-  translates :title, :description, :body
+  translates :title, :short_title, :description, :body
     
   def to_param
     title ? "#{id}-#{title.to_url}" : id
