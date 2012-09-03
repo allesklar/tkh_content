1ea814ceb3e27e8c3e9808027262a275e4c3b2ab
Swami Atma <swami@ohlalaweb.com>
2012-09-03 13:01:29 +0200

    Added comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,26 @@
+# this is needed for now to make mass assignment security compatible with the translation of globalize3
+Globalize::ActiveRecord::Translation.class_eval do
+  attr_accessible :locale
+end
+
+class Comment < ActiveRecord::Base
+  
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :page
+  
+  # non-accessible attributes: author_id, :status, :locale
+  attr_accessible :body, :page_id
+  
+  validates_presence_of :page_id
+  validates_presence_of :author_id
+  validates_presence_of :body
+  
+  scope :printable, where('status = ? OR status = ?', 'pending', 'accepted')
+  scope :pending, where('status = ?', 'pending')
+  scope :accepted, where('status = ?', 'accepted')
+  scope :blocked, where('status = ?', 'blocked')
+  scope :by_recent, order('updated_at desc')
+  scope :by_created, order('created_at')
+  scope :by_recently_created, order('created_at desc')
+  
+end
--- app/models/page.rb
+++ app/models/page.rb
@@ -6,6 +6,7 @@ end
 class Page < ActiveRecord::Base
   
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments, :dependent => :destroy
   
   attr_accessible :title, :short_title, :description, :body, :for_blog, :parent_id
   
