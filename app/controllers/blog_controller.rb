3c95642eea0102cc7d8e6b7b81bb8fabeaefba71
Swami Atma <swami@ohlalaweb.com>
2012-09-18 13:23:35 +0200

    Added blog RSS feed.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,6 +2,7 @@ class BlogController < ApplicationController
   
   def index
     @posts = Page.for_blog.published.order('published_at desc').paginate(:page => params[:page], :per_page => 20)
+    # respond_to  
     render :layout => 'blog'
   end
   
