ea9d99203f824f5f097f47d8371c5cc38d5cd9a5
Swami Atma <swami@ohlalaweb.com>
2012-09-19 13:46:58 +0200

    Switched html editor to tkh_inline_editor.

--- lib/tkh_content.rb
+++ lib/tkh_content.rb
@@ -5,7 +5,8 @@ require 'stringex'
 require 'globalize3'
 require 'will_paginate'
 require 'bootstrap-will_paginate'
-require 'bootstrap-wysihtml5-rails'
+# require 'bootstrap-wysihtml5-rails'
+require 'tkh_inline_editor'
 
 module TkhContent
   class Engine < ::Rails::Engine
