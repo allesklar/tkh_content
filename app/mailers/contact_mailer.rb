8a0a6e7e95698e56027f29ce009f90790e5e67a7
Swami Atma <swami@ohlalaweb.com>
2013-01-03 22:18:03 +0100

    Debugged the message from contact form recipient and form fields.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -8,7 +8,11 @@ class ContactMailer < ActionMailer::Base
   #   en.user_mailer.password_reset.subject
   #
   def message_from_contact_form(contact)  
-    @contact = contact  
-    mail to: 'swami@TenThousandHours.eu',  from: "\"#{@contact.sender_name}\" <#{@contact.sender_email}>" , subject: 'Message from the contact form'
+    @contact = contact
+    # FIXME - only the email addresses show up. not the names :-(
+    # TODO - what do to if contact_email is not set?
+    recipient = "#{Setting.first.company_name} <#{Setting.first.contact_email}>"
+    from = "#{@contact.sender_name} <#{@contact.sender_email}>"
+    mail to: recipient,  from: from , subject: 'Message from the contact form'
   end
 end
