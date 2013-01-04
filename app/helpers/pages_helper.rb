9009ccfefd80cbe0188326e530ad64533075c6b4
Swami Atma <swami@ohlalaweb.com>
2013-01-04 13:09:51 +0100

    Created DSL to enable admin users to embed contact form in any page.

---
+++ app/helpers/pages_helper.rb
@@ -0,0 +1,6 @@
+module PagesHelper
+  def tkhed(content)
+    content = content.gsub /\(-: contact_us_form :-\)/, render('contacts/form') if content.match(/(-: contact_us_form :-)/)
+    content
+  end
+end
