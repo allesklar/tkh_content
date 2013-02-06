75f6a84eb524a186e3c08f7420b039e383f2af67
Swami Atma <swami@ohlalaweb.com>
2013-02-06 20:42:39 +0100

    Added the dropin_class_schedule partial to the happy DSL.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,8 @@
 module PagesHelper
   def tkhed(content)
     content = content.gsub /\(-: contact_us_form :-\)/, render('contacts/form') if content.match(/\(-: contact_us_form :-\)/)
+    # from what will be the tkh_yoga_center_gem
+    content = content.gsub /\(-: dropin_class_schedule :-\)/, render('dropin_classes/schedule') if content.match(/\(-: dropin_class_schedule :-\)/)
     content
   end
 end
