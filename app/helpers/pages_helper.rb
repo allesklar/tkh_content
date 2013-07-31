c27001bf269caa5d8784e5b34ed1b61f39661ae1
Swami Atma <swami@ohlalaweb.com>
2013-07-31 15:15:55 +0200

    Added a variable for upcoming_events_list in pages helper.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,6 +3,8 @@ module PagesHelper
     content = content.gsub /\(-: contact_us_form :-\)/, render('contacts/form') if content.match(/\(-: contact_us_form :-\)/)
     # from what will be the tkh_yoga_center_gem
     content = content.gsub /\(-: dropin_class_schedule :-\)/, render('dropin_classes/schedule') if content.match(/\(-: dropin_class_schedule :-\)/)
+    # from what will be the tkh_upcoming_events_gem
+    content = content.gsub /\(-: upcoming_events_list :-\)/, render('events/upcoming_events_list') if content.match(/\(-: upcoming_events_list :-\)/)
     content
   end
 end
