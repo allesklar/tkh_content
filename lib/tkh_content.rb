2d16666677dac9c55c9a7c67acf2f4981cd6691e
Swami Atma <swami@ohlalaweb.com>
2013-11-09 11:08:26 +0100

    Refreshed gem dependencies.

--- lib/tkh_content.rb
+++ lib/tkh_content.rb
@@ -1,12 +1,10 @@
 require "tkh_content/version"
 require 'bootstrap-sass'
+require 'bootstrap-will_paginate'
 require 'simple_form'
 require 'stringex'
 require 'globalize'
 require 'will_paginate'
-require 'bootstrap-will_paginate'
-# require 'bootstrap-wysihtml5-rails'
-# require 'tkh_inline_editor'
 require 'ckeditor_rails'
 
 module TkhContent
