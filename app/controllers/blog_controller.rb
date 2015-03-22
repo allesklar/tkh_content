f7b7fc22979cd816c837df36be26218170525fdf
Swami Atma <swami@TenThousandHours.eu>
2015-03-22 14:49:02 +0100

    Debugged blog#by_tag method and view when there were no posts for a specific tag. Can happen with a non-translated bi-lingual post.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -9,11 +9,17 @@ class BlogController < ApplicationController
   end
 
   def by_tag
+    # this is necessary to deal with instances when no posts are present for that tag
+    @tag_name = params[:id]
     # this little hack is necessary because globalize won't let me access the tag directly by name
-    tag_id = Tag::Translation.find_by( locale: I18n.locale.to_s, name: params[:id] ).tag_id
-    @tag = Tag.find(tag_id)
-    @posts = @tag.pages.published.order('published_at desc').paginate(:page => params[:page], :per_page => 20)
-    render :layout => 'blog'
+    tag = Tag::Translation.find_by( locale: I18n.locale.to_s, name: @tag_name )
+    if tag.present?
+      @tag = Tag.find(tag.tag_id)
+      @posts = @tag.pages.published.order('published_at desc').paginate(:page => params[:page], :per_page => 20)
+      render :layout => 'blog'
+    else
+      @posts = nil
+    end
   end
 
 end
